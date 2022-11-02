af1857ee3257d305764077706ecbfe9245aadea1
farhanacsebd <farhana.cse.bd@gmail.com>
2022-11-02 23:29:50 +0600

    new start js code

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
-mins = document.querySelector('.mins');
-seconds = document.querySelector('.seconds');
-count = document.querySelector('.count');
-const btnStart = document.querySelector('.btn-start');
-const btnStop = document.querySelector('.btn-stop');
-const btnReset = document.querySelector('.btn-reset');
-
-
-let min = 0;
+let tan = 0;
 let sec = 0;
-let count = 0;
+let min = 0;
 
-
-let time = false;
-
-
-
-function start(){
-    
-}
-
-
-function stop(){
-
-}
-
-
-
-function reset(){
-
-}
