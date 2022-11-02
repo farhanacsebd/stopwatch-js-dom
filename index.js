6563877ada82694164ff2faa00adf5155b1e9682
farhanacsebd <farhana.cse.bd@gmail.com>
2022-11-02 20:56:51 +0600

    added html id

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-alert('hello')
+mins = document.querySelector('.mins');
+seconds = document.querySelector('.seconds');
+count = document.querySelector('.count');
+const btnStart = document.querySelector('.btn-start');
+const btnStop = document.querySelector('.btn-stop');
+const btnReset = document.querySelector('.btn-reset');
+
+
+let min = 0;
+let sec = 0;
+let count = 0;
+
+
+let time = false;
+
+
+
+function start(){
+    
+}
+
+
+function stop(){
+
+}
+
+
+
+function reset(){
+
+}
