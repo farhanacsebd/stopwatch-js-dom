fefcbaebc5c4c8d7d092c3bcb66301feaacc05de
Farhana Binte Hasan <63974588+farhanacsebd@users.noreply.github.com>
2022-11-03 18:34:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,3 @@
-/* let tans = 0;
-let secs = 0;
-let mins = 0;
-let getTan = document.querySelector('.tens');
-let getSec = document.querySelector('.seconds');
-let getMin = document.querySelector('.mins');
-let interval;
-
-function start(){
-    interval = setInterval(startTimer, 10)
-}
-
-function stop(){
-    clearInterval(interval);
-}
-function reset(){
-    clearInterval(interval);
-    tens = '00';
-    seconds = '00';
-    mins = '00';
-    getSeconds.innerHTML = seconds;
-    getTens.innerHTML = tens;
-    getMins.innerHTML = mins;
-}
-
-function startTimer(){
-    tans++;
-    if(tans <= 9){
-        getTan.innerHTML = '0' + tans;
-    }
-    if(tans > 9){
-        getTan.innerHTML = tans;
-    }
-    if(tans > 99){
-        secs++;
-        getSec.innerHTML = '0' + secs;
-        tans = 0;
-        getTan.innerHTML = '0' + 0;
-    }
-    if(secs <= 9){
-        getSec.innerHTML = '0' + secs;
-    }
-    if(secs > 9){
-        getSec.innerHTML = secs;
-    }
-    if(secs > 59){
-        mins ++;
-        getMin.innerHTML = '0' + mins;
-        secs = 0;
-        getSec.innerHTML = '0' + 0;
-    }
-    if(mins <= 9){
-        getMin.innerHTML = '0' + mins;
-    }
-} */
-
-
 let seconds = 00;
 let tens = 00;
 let mins = 00;
@@ -64,17 +7,20 @@ let getMins = document.querySelector('.mins');
 let btnStart = document.querySelector('.btn-start');
 let btnStop = document.querySelector('.btn-stop');
 let btnReset = document.querySelector('.btn-reset');
+
 let interval;
 
+
 btnStart.addEventListener('click', () => {
-    clearInterval(interval);
-    inverval = setInterval(startTimer, 10);
+    interval = setInterval(startTimer, 10)
 })
+
 btnStop.addEventListener('click', () => {
-    clearInterval(inverval);
+    clearInterval(interval);
 })
+
 btnReset.addEventListener('click', () => {
-    clearInterval(inverval);
+    clearInterval(interval);
     tens = '00';
     seconds = '00';
     mins = '00';
@@ -82,6 +28,10 @@ btnReset.addEventListener('click', () => {
     getTens.innerHTML = tens;
     getMins.innerHTML = mins;
 })
+
+
+
+// main function in here
 
 function startTimer(){
     tens++;
@@ -109,4 +59,4 @@ function startTimer(){
     if(mins > 9){
         getSeconds.innerHTML = mins;
     }
-}
+} 
